9d5442f3ba810da144fdd712688fe608ff824f7c
AlejandroAngelopoulos <alexander.angelopoulos.it@gmail.com>
2021-08-04 21:28:42 +0300

    4/8/2021

--- ResizeImageFromFolder/Form1.Designer.cs
+++ ResizeImageFromFolder/Form1.Designer.cs
@@ -43,50 +43,49 @@ namespace ResizeImageFromFolder
             this.label3 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
-            this.progressBarControl1 = new DevExpress.XtraEditors.ProgressBarControl();
-            ((System.ComponentModel.ISupportInitialize)(this.progressBarControl1.Properties)).BeginInit();
+            this.progressBarControl1 = new System.Windows.Forms.ProgressBar();
             this.SuspendLayout();
             // 
             // txtTF
             // 
-            this.txtTF.Location = new System.Drawing.Point(98, 77);
-            this.txtTF.Margin = new System.Windows.Forms.Padding(2);
+            this.txtTF.Location = new System.Drawing.Point(131, 95);
+            this.txtTF.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtTF.Name = "txtTF";
-            this.txtTF.Size = new System.Drawing.Size(320, 20);
+            this.txtTF.Size = new System.Drawing.Size(425, 22);
             this.txtTF.TabIndex = 0;
             // 
             // txtSF
             // 
-            this.txtSF.Location = new System.Drawing.Point(98, 43);
-            this.txtSF.Margin = new System.Windows.Forms.Padding(2);
+            this.txtSF.Location = new System.Drawing.Point(131, 53);
+            this.txtSF.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtSF.Name = "txtSF";
-            this.txtSF.Size = new System.Drawing.Size(320, 20);
+            this.txtSF.Size = new System.Drawing.Size(425, 22);
             this.txtSF.TabIndex = 1;
             // 
             // txtH
             // 
             this.txtH.BackColor = System.Drawing.Color.White;
             this.txtH.Enabled = false;
-            this.txtH.Location = new System.Drawing.Point(303, 130);
-            this.txtH.Margin = new System.Windows.Forms.Padding(2);
+            this.txtH.Location = new System.Drawing.Point(404, 160);
+            this.txtH.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtH.Name = "txtH";
-            this.txtH.Size = new System.Drawing.Size(76, 20);
+            this.txtH.Size = new System.Drawing.Size(100, 22);
             this.txtH.TabIndex = 2;
             // 
             // txtW
             // 
-            this.txtW.Location = new System.Drawing.Point(148, 130);
-            this.txtW.Margin = new System.Windows.Forms.Padding(2);
+            this.txtW.Location = new System.Drawing.Point(197, 160);
+            this.txtW.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtW.Name = "txtW";
-            this.txtW.Size = new System.Drawing.Size(76, 20);
+            this.txtW.Size = new System.Drawing.Size(100, 22);
             this.txtW.TabIndex = 3;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(444, 43);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Location = new System.Drawing.Point(592, 53);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(56, 19);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 4;
             this.button1.Text = "Browse";
             this.button1.UseVisualStyleBackColor = true;
@@ -94,10 +93,10 @@ namespace ResizeImageFromFolder
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(444, 77);
-            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Location = new System.Drawing.Point(592, 95);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(56, 19);
+            this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 5;
             this.button2.Text = "Browse";
             this.button2.UseVisualStyleBackColor = true;
@@ -105,10 +104,10 @@ namespace ResizeImageFromFolder
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(207, 160);
-            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Location = new System.Drawing.Point(276, 197);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(102, 37);
+            this.button3.Size = new System.Drawing.Size(136, 46);
             this.button3.TabIndex = 6;
             this.button3.Text = "Resize and Save to folder";
             this.button3.UseVisualStyleBackColor = true;
@@ -117,40 +116,36 @@ namespace ResizeImageFromFolder
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 46);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(21, 57);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.Size = new System.Drawing.Size(88, 16);
             this.label1.TabIndex = 8;
             this.label1.Text = "Select Folder";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(32, 80);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(43, 98);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.Size = new System.Drawing.Size(67, 16);
             this.label2.TabIndex = 9;
             this.label2.Text = "To Folder";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(92, 132);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(123, 162);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.Size = new System.Drawing.Size(45, 16);
             this.label3.TabIndex = 10;
             this.label3.Text = "Width ";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(246, 132);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(328, 162);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.Size = new System.Drawing.Size(47, 16);
             this.label5.TabIndex = 12;
             this.label5.Text = "Height";
             // 
@@ -164,24 +159,18 @@ namespace ResizeImageFromFolder
             // 
             // progressBarControl1
             // 
-            this.progressBarControl1.Location = new System.Drawing.Point(95, 227);
+            this.progressBarControl1.Location = new System.Drawing.Point(131, 273);
             this.progressBarControl1.Name = "progressBarControl1";
-            this.progressBarControl1.Properties.Appearance.BackColor = System.Drawing.Color.Transparent;
-            this.progressBarControl1.Properties.EndColor = System.Drawing.Color.SteelBlue;
-            this.progressBarControl1.Properties.LookAndFeel.Style = DevExpress.LookAndFeel.LookAndFeelStyle.UltraFlat;
-            this.progressBarControl1.Properties.LookAndFeel.UseDefaultLookAndFeel = false;
-            this.progressBarControl1.Properties.ProgressViewStyle = DevExpress.XtraEditors.Controls.ProgressViewStyle.Solid;
-            this.progressBarControl1.Properties.ShowTitle = true;
-            this.progressBarControl1.Properties.StartColor = System.Drawing.Color.PowderBlue;
-            this.progressBarControl1.Properties.Step = 1;
-            this.progressBarControl1.Size = new System.Drawing.Size(323, 18);
-            this.progressBarControl1.TabIndex = 15;
+            this.progressBarControl1.Size = new System.Drawing.Size(425, 23);
+            this.progressBarControl1.Step = 1;
+            this.progressBarControl1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBarControl1.TabIndex = 13;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(528, 257);
+            this.ClientSize = new System.Drawing.Size(704, 316);
             this.Controls.Add(this.progressBarControl1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label3);
@@ -194,14 +183,13 @@ namespace ResizeImageFromFolder
             this.Controls.Add(this.txtH);
             this.Controls.Add(this.txtSF);
             this.Controls.Add(this.txtTF);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.progressBarControl1.Properties)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -221,7 +209,7 @@ namespace ResizeImageFromFolder
         private Label label3;
         private Label label5;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
-        private DevExpress.XtraEditors.ProgressBarControl progressBarControl1;
+        private ProgressBar progressBarControl1;
     }
 }
 
